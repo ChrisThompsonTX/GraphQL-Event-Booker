294081b4e56976da03a57e0831e824213378de90
Chris Thompson <chris.tx86@gmail.com>
2020-10-08 21:37:42 -0700

    add graphql

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('sup')
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        schema {
+            query:
+            mutation:
+        }
+    `),
+    rootValue: {}
+}));
 
 app.listen(3000);
